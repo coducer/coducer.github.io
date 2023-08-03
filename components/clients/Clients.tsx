3129e90746d9332201f3e6ce9f52dff26f0c16ba
Jebaz A <jebas@coducer.com>
2023-08-03 19:31:56 +0530

    impactree logo added

--- components/clients/Clients.tsx
+++ components/clients/Clients.tsx
@@ -6,7 +6,7 @@ import cl3 from "../../public/assets/impactreelogo.png";
 import cl4 from "../../public/assets/alar.png";
 import cl5 from "../../public/assets/ihx.png";
 import cl6 from "../../public/assets/cl6.png";
-import impactree from "../../public/assets/Impactree.png"
+import impactree from "../../public/assets/impactreelogo.png"
 import { Container } from "react-bootstrap";
 import Image from "next/image";
 const Clients = () => {
@@ -14,15 +14,18 @@ const Clients = () => {
     <>
       <div className="clients_screen">
         <Container>
-            {/* <div className="process_clients"> */}
-              <div className="process_title_clients text-center py-3">We are Working with</div>
-            {/* </div> */}
+          {/* <div className="process_clients"> */}
+          <div className="process_title_clients text-center py-3">We are Working with</div>
+          {/* </div> */}
 
           <div className="slider">
             <div className="slide-track">
               <div className="slide">
                 <Image src={cl2} height="100" width="250" alt="" />
               </div>
+              <div className="slide">
+                <Image src={impactree} width="100" alt="" />
+              </div>
               <div className="slide me-3">
                 <Image src={cl4} height="100" width="150" alt="" />
               </div>
@@ -39,6 +42,9 @@ const Clients = () => {
                 <Image src={cl5} height="50" width="150" alt="" />
               </div>
               <div className="slide">
+                <Image src={impactree} width="100" alt="" />
+              </div>
+              <div className="slide">
                 <Image src={cl2} height="100" width="250" alt="" />
               </div>
               <div className="slide me-3">
@@ -47,7 +53,7 @@ const Clients = () => {
               <div className="slide me-3">
                 <Image src={cl5} height="50" width="150" alt="" />
               </div>
-                          </div>
+            </div>
           </div>
         </Container>
       </div>
