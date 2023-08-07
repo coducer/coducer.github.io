641227ee3e65013c5080103514a0adbbd03178b3
Jaikeerthi K <jai@coducer.com>
2023-08-07 16:31:26 +0530

    remove footer from blogs

--- app/blogs/superhero/page.tsx
+++ app/blogs/superhero/page.tsx
@@ -1,76 +1,135 @@
-'use client'
-import Navbar from "../../../components/navbar/Navbar";
-import Footer from "../../../components/footer/Footer";
-import React from "react";
+"use client";
+import Navbar from "@/components/navbar/Navbar";
 import { Container } from "react-bootstrap";
 
 const page = () => {
-    return (
-        <div>
-            <Navbar />
-            <div>
-                <Container>
-                    <div className="scope py-3">
-                        <p className="blog-content">
-                            <p>
-
-                                <b>Journey to Tech Excellence</b>
-                                <b>A Superhero Founder's comprehensive supertools guide to build an MVP</b>
-
-                                <p>It was a sunny afternoon in Innovateville and Alex, the Superhero entrepreneur was curious about how an MVP is crafted within 100 days. Having learnt from his tech friend 100 Days of MVP about the extensive processes involved, Alex was yearning to know the secret strategy employed in choosing the right tools and technology that make his product special.
-                                The genie appeared from a cloud of magical smoke in the cracked wall of his room like the last time and greeted Alex with a warm smile. Alex was all ears to listen to the genie’s expatiation on the tools and technologies used to build a remarkable MVP.</p>
-
-
-                                <b>Tools used for building an MVP</b>
-
-                                <p>“O noble entrepreneur, having meticulously crafted the process, equal attention is given to choosing appropriate tools that align with the needs of the product. The end goal of an MVP is to quickly test the core concept and gather feedback from early users while keeping development costs and time to a minimum.” said the genie with a smirk.
-                                “But genie, this seems to be a daunting task!” remarked Alex.
-                                ‘Well, not if we have a distinctly defined set of tools classified based on its purpose” replied the genie.
-                                “That’s tricky! What purpose does each tool serve?” quipped Alex.
-                                And the genie began its sermon. “The choice of tools will depend on the project’s requirements and budget. The tools mentioned help to expedite the development process and validate ideas faster.</p>
-                                
-                                <b>Function and Purpose of Tools :</b>
-
-                                1. Customer Interview Tools  - TypeForm, Google Forms, LinkedIn
-                                2. Programming Languages and Frameworks: Depending on the nature of your product, you'll need to choose, it can accelerate the development process by providing reusable components and a structured approach to building the user interface. Popular choices include Python (Django or Flask), Ruby on Rails, Ruby(Sintara), Node.js, Django, React, Angular, Vue.js etc are other options for building the back-end, depending on the language and framework you are comfortable with.
-                                3. Mobile App Development - React Native or Flutter: These frameworks allow you to develop cross-platform mobile apps, which can significantly reduce the development time and effort.
-                                4. Version Control Systems: Version control systems like Git are essential for tracking changes to your codebase, collaborating with team members, and rolling back changes if necessary.
-                                5. Web Development Tools: If you're building a web-based MVP, you'll need tools like HTML, CSS, JavaScript, and libraries or frameworks like Bootstrap, jQuery, or Vue.js to create the user interface. Building a simple web application or landing page can be done using these foundational web technologies.
-                                6. Heroku, Netlify: These platforms simplify the deployment process, especially for web applications.
-                                7. Backend-as-a-Service (BaaS) Platforms: BaaS platforms like Firebase, AWS Amplify, or Backendless can speed up development by providing ready-to-use backend services, such as user authentication, databases, and cloud storage. Node.js: A popular choice for building the back-end using JavaScript allows you to use the same language on both the front-end and back-end, facilitating faster development.
-                                8. Prototyping Tools: Tools like Sketch, Figma, or Adobe XD help you create interactive mockups and prototypes of your product's user interface and user experience (UI/UX) , allowing you to visualise and iterate on design concepts before development.
-                                9. Tools to build products with good user experience - Balsamiq, Webflow, Marvel
-                                10. Native Development: If your MVP requires maximum performance or platform-specific features, you might opt for native development for iOS (Swift) and Android (Java/Kotlin).
-                                11. Database Management Systems: For data storage and management, you may use popular database management systems like MySQL, PostgreSQL, MongoDB, or Firebase Realtime Database, depending on your project's requirements. Choose one based on your data requirements and familiarity with the technology.
-                                12. Cloud Services: Cloud platforms like Amazon Web Services (AWS), Microsoft Azure, or Google Cloud Platform (GCP) offer various services for hosting, scaling, and managing your application. These cloud service providers offer scalable hosting solutions for your application.
-                                13. Tools to build landing pages for MVP - InstaPage, Strikingly, QuickMVP
-                                14. Analytics and Tracking Tools: To gather insights and track user behaviour, you can integrate tools like Google Analytics or Mixpanel, HotJar into your MVP. Allows you to track user behaviour and gain insights into user interactions with your MVP.
-                                15. MVP launch tools - Reddit /r/startups, ProductHunt, BetaList
-                                16. Payment and Subscription Integration: If your MVP involves transactions or subscriptions, services like Stripe or PayPal can help you handle payments securely.
-                                17. Communication and Collaboration Tools: Tools like Slack, Trello, Microsoft Teams or Asana aid communication and project management, allowing your team to stay organised and work efficiently during the development process.
-                                18. Continuous Integration and Deployment (CI/CD) Tools: CI/CD tools like Jenkins, CircleCI, or GitHub Actions enable automated testing, building, and deployment of your MVP, reducing the time between development and production releases.
-                                19. Customer Support and Feedback: Implementing tools like Intercom or Drift can help you communicate with early users, collect feedback, and address their inquiries.
-
-
-                                “Woah! That's an exhaustive list, ” remarked Alex.
-                                “It is quite an extensive list ” smiled the genie. “Also one must not forget to employ the right methodology Alex”
-                                “Methodology? What is it?” questioned Alex.
-                                “Well I promise to talk about it in our next meeting”. The genie bid farewell and disappeared.
-                                With the newfound knowledge, a talented development team by his side, and an abundance of inspiration, Alex embarked on his entrepreneurial journey with the confidence and tools needed to build an extraordinary MVP for his startup. And so, with the genie's assistance, Alex's dreams began to transform into reality, leaving a trail of success and innovation in its wake.
-
-
-                                To be continued…
-
-
-
-                            </p>
-                        </p>
-                    </div>
-                </Container>
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <Container>
+          <div className="scope py-3">
+            <p className="blog-content">
+              <p>
+                <b>Journey to Tech Excellence</b>
+                <b>
+                  A Superhero Founder's comprehensive supertools guide to build
+                  an MVP
+                </b>
+                <p>
+                  It was a sunny afternoon in Innovateville and Alex, the
+                  Superhero entrepreneur was curious about how an MVP is crafted
+                  within 100 days. Having learnt from his tech friend 100 Days
+                  of MVP about the extensive processes involved, Alex was
+                  yearning to know the secret strategy employed in choosing the
+                  right tools and technology that make his product special. The
+                  genie appeared from a cloud of magical smoke in the cracked
+                  wall of his room like the last time and greeted Alex with a
+                  warm smile. Alex was all ears to listen to the genie’s
+                  expatiation on the tools and technologies used to build a
+                  remarkable MVP.
+                </p>
+                <b>Tools used for building an MVP</b>
+                <p>
+                  “O noble entrepreneur, having meticulously crafted the
+                  process, equal attention is given to choosing appropriate
+                  tools that align with the needs of the product. The end goal
+                  of an MVP is to quickly test the core concept and gather
+                  feedback from early users while keeping development costs and
+                  time to a minimum.” said the genie with a smirk. “But genie,
+                  this seems to be a daunting task!” remarked Alex. ‘Well, not
+                  if we have a distinctly defined set of tools classified based
+                  on its purpose” replied the genie. “That’s tricky! What
+                  purpose does each tool serve?” quipped Alex. And the genie
+                  began its sermon. “The choice of tools will depend on the
+                  project’s requirements and budget. The tools mentioned help to
+                  expedite the development process and validate ideas faster.
+                </p>
+                <b>Function and Purpose of Tools :</b>
+                1. Customer Interview Tools - TypeForm, Google Forms, LinkedIn
+                2. Programming Languages and Frameworks: Depending on the nature
+                of your product, you'll need to choose, it can accelerate the
+                development process by providing reusable components and a
+                structured approach to building the user interface. Popular
+                choices include Python (Django or Flask), Ruby on Rails,
+                Ruby(Sintara), Node.js, Django, React, Angular, Vue.js etc are
+                other options for building the back-end, depending on the
+                language and framework you are comfortable with. 3. Mobile App
+                Development - React Native or Flutter: These frameworks allow
+                you to develop cross-platform mobile apps, which can
+                significantly reduce the development time and effort. 4. Version
+                Control Systems: Version control systems like Git are essential
+                for tracking changes to your codebase, collaborating with team
+                members, and rolling back changes if necessary. 5. Web
+                Development Tools: If you're building a web-based MVP, you'll
+                need tools like HTML, CSS, JavaScript, and libraries or
+                frameworks like Bootstrap, jQuery, or Vue.js to create the user
+                interface. Building a simple web application or landing page can
+                be done using these foundational web technologies. 6. Heroku,
+                Netlify: These platforms simplify the deployment process,
+                especially for web applications. 7. Backend-as-a-Service (BaaS)
+                Platforms: BaaS platforms like Firebase, AWS Amplify, or
+                Backendless can speed up development by providing ready-to-use
+                backend services, such as user authentication, databases, and
+                cloud storage. Node.js: A popular choice for building the
+                back-end using JavaScript allows you to use the same language on
+                both the front-end and back-end, facilitating faster
+                development. 8. Prototyping Tools: Tools like Sketch, Figma, or
+                Adobe XD help you create interactive mockups and prototypes of
+                your product's user interface and user experience (UI/UX) ,
+                allowing you to visualise and iterate on design concepts before
+                development. 9. Tools to build products with good user
+                experience - Balsamiq, Webflow, Marvel 10. Native Development:
+                If your MVP requires maximum performance or platform-specific
+                features, you might opt for native development for iOS (Swift)
+                and Android (Java/Kotlin). 11. Database Management Systems: For
+                data storage and management, you may use popular database
+                management systems like MySQL, PostgreSQL, MongoDB, or Firebase
+                Realtime Database, depending on your project's requirements.
+                Choose one based on your data requirements and familiarity with
+                the technology. 12. Cloud Services: Cloud platforms like Amazon
+                Web Services (AWS), Microsoft Azure, or Google Cloud Platform
+                (GCP) offer various services for hosting, scaling, and managing
+                your application. These cloud service providers offer scalable
+                hosting solutions for your application. 13. Tools to build
+                landing pages for MVP - InstaPage, Strikingly, QuickMVP 14.
+                Analytics and Tracking Tools: To gather insights and track user
+                behaviour, you can integrate tools like Google Analytics or
+                Mixpanel, HotJar into your MVP. Allows you to track user
+                behaviour and gain insights into user interactions with your
+                MVP. 15. MVP launch tools - Reddit /r/startups, ProductHunt,
+                BetaList 16. Payment and Subscription Integration: If your MVP
+                involves transactions or subscriptions, services like Stripe or
+                PayPal can help you handle payments securely. 17. Communication
+                and Collaboration Tools: Tools like Slack, Trello, Microsoft
+                Teams or Asana aid communication and project management,
+                allowing your team to stay organised and work efficiently during
+                the development process. 18. Continuous Integration and
+                Deployment (CI/CD) Tools: CI/CD tools like Jenkins, CircleCI, or
+                GitHub Actions enable automated testing, building, and
+                deployment of your MVP, reducing the time between development
+                and production releases. 19. Customer Support and Feedback:
+                Implementing tools like Intercom or Drift can help you
+                communicate with early users, collect feedback, and address
+                their inquiries. “Woah! That's an exhaustive list, ” remarked
+                Alex. “It is quite an extensive list ” smiled the genie. “Also
+                one must not forget to employ the right methodology Alex”
+                “Methodology? What is it?” questioned Alex. “Well I promise to
+                talk about it in our next meeting”. The genie bid farewell and
+                disappeared. With the newfound knowledge, a talented development
+                team by his side, and an abundance of inspiration, Alex embarked
+                on his entrepreneurial journey with the confidence and tools
+                needed to build an extraordinary MVP for his startup. And so,
+                with the genie's assistance, Alex's dreams began to transform
+                into reality, leaving a trail of success and innovation in its
+                wake. To be continued…
+              </p>
+            </p>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default page;
