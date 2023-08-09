55c4dc66a7303b159ab6addc8f1ade8abc244a62
Jebaz A <jebas@coducer.com>
2023-08-09 19:12:11 +0530

    font size changed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -18,8 +18,8 @@ export default function Home() {
       <Circle />
       <Details />
       <Work />
-      {/* <Testimonial />
-      <Team /> */}
+      {/* <Testimonial /> */}
+      {/* <Team /> */}
       <Clients />
       <Faq />
     </>
