9ae384f7a51671b1acd0f7efe2d29ce2556ec10d
usamabinrafiq15 <usama@coducer.com>
2023-08-14 20:37:36 +0530

    hamburger icon fixed for surface pro 7

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,21 +6,23 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="footer_screen py-5">
-      <Container>
-        <div className="footer py-2 ">
-          <div className="company_logo d-flex align-items-center py-3 flex-grow-1">
-            <Image src={company_logo} alt="" />
-          </div>
-          <div className="footer_description  py-3">
-            <div className="copy_rights">
-              <span>Coducer Technologies &#169; 2023</span>
-              <span className="border_left">All Rights Reserved</span>
+    <>
+      <div className="footer_screen py-5">
+        <Container>
+          <div className="footer py-2 ">
+            <div className="company_logo d-flex align-items-center py-3 flex-grow-1">
+              <Image src={company_logo} alt="" />
+            </div>
+            <div className="footer_description  py-3">
+              <div className="copy_rights">
+                <span>Coducer Technologies &#169; 2023</span>
+                <span className="border_left">All Rights Reserved</span>
+              </div>
             </div>
           </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </>
   );
 };
 
