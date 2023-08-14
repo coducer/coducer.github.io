df0c7158bc36eefc1d9c4c8a5e4a08a10b0f1de1
Jaikeerthi K <jai@coducer.com>
2023-08-14 12:31:44 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WPWZR47RGN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WPWZR47RGN');
+</script>
       </body>
     </html>
   );
