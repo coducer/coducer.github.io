8760e49656515ecbf0572b98312d59d46343f51b
Jebaz A <jebas@coducer.com>
2023-08-18 12:00:56 +0530

    commented footer

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,7 +4,7 @@ import Circle from "@/components/circle/Circle";
 import Clients from "@/components/clients/Clients";
 import Details from "@/components/details/Details";
 import Faq from "@/components/faq/Faq";
-import Footer from "@/components/footer/footer";
+// import Footer from "@/components/footer/footer";
 import Header from "@/components/header/Header";
 import Work from "@/components/latestWork/Work";
 import Navbar from "@/components/navbar/Navbar";
@@ -23,7 +23,7 @@ export default function Home() {
       {/* <Team /> */}
       <Clients />
       <Faq />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
