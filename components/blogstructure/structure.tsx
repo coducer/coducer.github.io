f20eeb44047e98621392c45be42b3eb62ce2722c
Jebaz A <jebas@coducer.com>
2023-08-21 18:38:19 +0530

    logo size changed and blog redirected to linkedin

--- components/blogstructure/structure.tsx
+++ components/blogstructure/structure.tsx
@@ -6,9 +6,11 @@ import Author from "../../public/assets/Indira.jpeg"
 import Mvp from "../../public/assets/blog_images/1 (2).png"
 import Tools from "../../public/assets/blog_images/3-1.png"
 import Agile from "../../public/assets/blog_images/2 (1).png"
+import Vision from "../../public/assets/blog_images/2-1.png"
 
 
 const Structure = () => {
+    let today = new Date().toISOString().slice(0, 10)
     return (
         <div className="section-blog" style={{ minHeight: "100vh" }}>
             <Container>
@@ -20,7 +22,7 @@ const Structure = () => {
 
                 <div className="blog-row row pb-5">
                     <div className="blog-item col-lg-4">
-                        <a href="blogs/100daysofmvp" className="text-decoration-none text-black">
+                        <a href="https://www.linkedin.com/pulse/how-non-tech-superhero-founder-can-transform-revolutionising%3FtrackingId=%252BOERCuRmQuq4SbQ2PG9u5Q%253D%253D/?trackingId=%2BOERCuRmQuq4SbQ2PG9u5Q%3D%3D" target="_blank" className="text-decoration-none text-black">
                             <div className="blog-img">
                                 <Image src={Mvp} alt="MVP landing" className="blog-img-box" />
                             </div>
@@ -33,40 +35,20 @@ const Structure = () => {
                                 <div className="auth-img"><Image src={Author} alt="author-image" width={30} /></div>
                                 <div className="px-2">
                                     <div className="author-name">Indira</div>
-                                    <div className="publish-date">January 01,2023</div>
+                                    <div className="publish-date">{today }</div>
                                 </div>
                             </div>
                         </a>
                     </div>
 
-                    <div className="blog-item col-lg-4">
-                        <a href="blogs/superhero" className="text-decoration-none text-black">
-                            <div className="blog-img">
-                                <Image src={Tools} alt="MVP landing" className="blog-img-box" />
-                            </div>
-                            <div className="blog-details">
-                                <div className="blog-genre pt-3">Tools</div>
-                                <div className="blog-title py-3">Tools used for building MVP</div>
-                                <div className="blog-oneliner">Explore essential tools and technologies that streamline MVP development, enabling efficient prototyping, testing, and validation of your ideas.</div>
-                            </div>
-                            <div className="author-info align-items-center pt-3">
-                                <div className="auth-img"><Image src={Author} alt="author-image" width={30} /></div>
-                                <div className="px-2">
-                                    <div className="author-name">Indira</div>
-                                    <div className="publish-date">January 01,2023</div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="blog-item col-lg-4">
+                    {/* <div className="blog-item col-lg-4">
                         <a href="blogs/superhero" className="text-decoration-none text-black">
                             <div className="blog-img">
                                 <Image src={Agile} alt="MVP landing" className="blog-img-box" />
                             </div>
                             <div className="blog-details">
-                                <div className="blog-genre pt-3">Tools</div>
-                                <div className="blog-title py-3">Tools used for building MVP</div>
+                                <div className="blog-genre pt-3">Story</div>
+                                <div className="blog-title py-3">Journey to Tech Excellence</div>
                                 <div className="blog-oneliner">Explore essential tools and technologies that streamline MVP development, enabling efficient prototyping, testing, and validation of your ideas.</div>
                             </div>
                             <div className="author-info align-items-center pt-3">
@@ -78,19 +60,16 @@ const Structure = () => {
                             </div>
                         </a>
                     </div>
-                </div>
 
-
-                <div className="blog-row row mb-5">
                     <div className="blog-item col-lg-4">
                         <a href="blogs/vision" className="text-decoration-none text-black">
                             <div className="blog-img">
-                                <Image src={Agile} alt="MVP landing" className="blog-img-box" />
+                                <Image src={Vision} alt="MVP landing" className="blog-img-box" />
                             </div>
                             <div className="blog-details">
-                                <div className="blog-genre pt-3">Tools</div>
-                                <div className="blog-title py-3">Tools used for building MVP</div>
-                                <div className="blog-oneliner">Explore essential tools and technologies that streamline MVP development, enabling efficient prototyping, testing, and validation of your ideas.</div>
+                                <div className="blog-genre pt-3">Story</div>
+                                <div className="blog-title py-3">From Vision to MVP</div>
+                                <div className="blog-oneliner">Uncover the hurdles non-tech founders confront when crafting an MVP in 100 days, and find actionable solutions in the upcoming blog.</div>
                             </div>
                             <div className="author-info align-items-center pt-3">
                                 <div className="auth-img"><Image src={Author} alt="author-image" width={30} /></div>
@@ -100,8 +79,13 @@ const Structure = () => {
                                 </div>
                             </div>
                         </a>
-                    </div>
+                    </div> */}
                 </div>
+
+{/* 
+                <div className="blog-row row mb-5">
+                    
+                </div> */}
             </Container>
         </div>
     )
--- components/latestWork/Work.tsx
+++ components/latestWork/Work.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Image from "next/image";
 import Jofin from "../../public/assets/mockups/Jofin (1).png";
-import Jfnlgo from "../../public/assets/jfn.png";
+import Jfnlgo from "../../public/assets/Jofin_48.png";
 import AA from "../../public/assets/mockups/Al Arabia.png";
-import Ala from "../../public/assets/Al Arabia.png";
+import Ala from "../../public/assets/Al Arabia 48px.png";
 import Carousel from 'react-bootstrap/Carousel';
-import Impact from '../../public/assets/impactreelogo.png'
+import Impact from '../../public/assets/Impactree_48.png'
 import Impactree from '../../public/assets/mockups/Impactree.png'
 
 const Work = () => {
@@ -22,62 +22,16 @@ const Work = () => {
                 Quick analysis behind our stellar MVP launches
               </p>
             </div>
-
+            
               <div className="row wrk-sample">
-{/* 
-                <div className="content col-lg-4">
-                  <a href="https://www.jofin.ai/" target="_blank">
-                    <div className="content-overlay"></div>
-                    <Image
-                      className="content-image working"
-                      src={mock3}
-                      alt=""
-                    />
-                    <div className="content-details fadeIn-bottom">
-                      <h3 className="content-title">Jofin</h3>
-                    </div>
-                  </a>
-                </div>
-
-                <div className="content col-lg-4">
-                  <a
-                    href="http://prabhaav-user.coducer.xyz/login"
-                    target="_blank"
-                  >
-                    <div className="content-overlay"></div>
-                    <Image
-                      className="content-image working"
-                      src={mock1}
-                      alt=""
-                    />
-                    <div className="content-details fadeIn-bottom">
-                      <h3 className="content-title">Impactree</h3>
-                    </div>
-                  </a>
-                </div>
-
-                <div className="content col-lg-4">
-                  <a href="http://20.74.152.254/login" target="_blank">
-                    <div className="content-overlay"></div>
-                    <Image
-                      className="content-image working"
-                      src={mock2}
-                      alt=""
-                    />
-                    <div className="content-details fadeIn-bottom">
-                      <h3 className="content-title">Al-Arabia</h3>
-                    </div>
-                  </a>
-                </div> */}
-
                  <Carousel>
                   <Carousel.Item interval={5000} >
                     <div className="row mt-3" style={{ maxHeight:"50vh"}}>
                       <div className="col-md-5">
                         <div className="d-flex justify-content-center align-items-center">
                           <div>
-                            <Image src={Jfnlgo} alt="" />
-                            <p className="pb-0 mb-0 pt-5">Jofin</p>
+                            <Image src={Jfnlgo} alt="" className="mb-5"/>
+                            {/* <p className="pb-0 mb-0 pt-5">Jofin</p> */}
                             <h4 className="fs-1 fw-bold">Jofin</h4>
                             <p>Jofin provides comprehensive personal financial management solutions. Comprising competent financial experts, provides unbiased financial advice right from how to plan, where to invest, which insurance to buy; besides providing additional facilities and features such as e-Tax filing, loans, and digital document locker all within one platform.</p>
                           </div>
@@ -108,8 +62,8 @@ const Work = () => {
                       <div className="col-md-5">
                         <div className="d-flex justify-content-center align-items-center">
                           <div>
-                            <Image src={Ala} alt="" />
-                            <p className="pb-0 mb-0 pt-5">Alarabia</p>
+                            <Image src={Ala} alt="" className="mb-5"/>
+                            {/* <p className="pb-0 mb-0 pt-5">Alarabia</p> */}
                             <h4 className="fs-1 fw-bold">Alarabia</h4>
                             <p>AlArabia has been the leading outdoor solution provider since 1983, offering clients with expertise across a multitude of platforms and disciplines. It has invested in companies across the entire media funnel to ensure quality from inception to delivery.</p>
                           </div>
@@ -140,8 +94,8 @@ const Work = () => {
                       <div className="col-md-5">
                         <div className="d-flex justify-content-center align-items-center">
                           <div>
-                            <Image src={Impact} alt="" />
-                            <p className="pb-0 mb-0 pt-5">Impactree</p>
+                            <Image src={Impact} alt="" className="mb-5"/>
+                            {/* <p className="pb-0 mb-0 pt-5">Impactree</p> */}
                             <h4 className="fs-1 fw-bold">Impactree</h4>
                             <p>A tech company specializing in social impact and sustainability providing products and solutions thet facilitate enterprise growth and value creation throughout their lifecycle, whether by boosting revenue, reducing costs, or expanding their reach.</p>
                           </div>
