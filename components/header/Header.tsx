6d026a17efaa6020ae6b43a0c7fc1125d177794b
Jebaz A <jebas@coducer.com>
2023-08-23 14:32:16 +0530

    vision spelling changed

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Image from "next/image";
-import Rocket from "../../public/assets/bg_startup.png";
+import Rocket from "../../public/assets/MVP.png";
 import { Sora } from "next/font/google";
 
 const Header = () => {
