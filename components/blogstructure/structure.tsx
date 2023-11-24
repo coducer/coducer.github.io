ff25134808fbd97c1fa7d5cc1b195a8675cc5d25
Nithish <nithish.n@coducer.com>
2023-11-24 11:47:58 +0530

    blog 2 added

--- components/blogstructure/structure.tsx
+++ components/blogstructure/structure.tsx
@@ -4,9 +4,8 @@ import Image from "next/image";
 import { Container } from "react-bootstrap";
 import Author from "../../public/assets/Indira.jpeg"
 import Mvp from "../../public/assets/blog_images/1 (2).png"
-import Tools from "../../public/assets/blog_images/3-1.png"
-import Agile from "../../public/assets/blog_images/2 (1).png"
-import Vision from "../../public/assets/blog_images/2-1.png"
+import Mvp_blog2 from "../../public/assets/startup-3-1024x525.jpg"
+
 
 
 const Structure = () => {
@@ -14,13 +13,11 @@ const Structure = () => {
     return (
         <div className="section-blog" style={{ minHeight: "100vh" }}>
             <Container>
-
                 <div className="text-center py-5">
                     <div className="blog-page-header">Our Latest Blog Posts</div>
-                    {/* <div className="blog-page-subheader"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></div> */}
                 </div>
 
-                <div className="blog-row row pb-5">
+                <div className="row pb-5">
                     <div className="blog-item col-lg-4">
                         <a href="https://www.linkedin.com/pulse/how-non-tech-superhero-founder-can-transform-revolutionizing%3FtrackingId=LhXalP6UVCV7sEfVo%252FCYEg%253D%253D/?trackingId=LhXalP6UVCV7sEfVo%2FCYEg%3D%3D" target="_blank" className="text-decoration-none text-black">
                             <div className="blog-img">
@@ -35,57 +32,37 @@ const Structure = () => {
                                 <div className="auth-img"><Image src={Author} alt="author-image" width={30} /></div>
                                 <div className="px-2">
                                     <div className="author-name">Indira</div>
-                                    <div className="publish-date">{today }</div>
+                                    <div className="publish-date">{today}</div>
                                 </div>
                             </div>
                         </a>
                     </div>
 
-                    {/* <div className="blog-item col-lg-4">
-                        <a href="blogs/superhero" className="text-decoration-none text-black">
-                            <div className="blog-img">
-                                <Image src={Agile} alt="MVP landing" className="blog-img-box" />
-                            </div>
-                            <div className="blog-details">
-                                <div className="blog-genre pt-3">Story</div>
-                                <div className="blog-title py-3">Journey to Tech Excellence</div>
-                                <div className="blog-oneliner">Explore essential tools and technologies that streamline MVP development, enabling efficient prototyping, testing, and validation of your ideas.</div>
-                            </div>
-                            <div className="author-info align-items-center pt-3">
-                                <div className="auth-img"><Image src={Author} alt="author-image" width={30} /></div>
-                                <div className="px-2">
-                                    <div className="author-name">Indira</div>
-                                    <div className="publish-date">January 01,2023</div>
+                    <div className="col-lg-4">
+                        <div className="blog-item ">
+                            <a href="https://www.linkedin.com/pulse/from-startup-idea-wow-mvp-story-explained-100daysofmvp-3vx9f?trk=public_post_embed_feed-article-content" target="_blank" className="text-decoration-none text-black">
+                                <div className="blog-img">
+                                    <Image src={Mvp_blog2} alt="MVP landing" className="blog-img-box" height={100} />
                                 </div>
-                            </div>
-                        </a>
+                                <div className="blog-details mt-5 pt-2">
+                                    <div className="blog-genre pt-3">Story</div>
+                                    <div className="blog-title py-3">From Startup Idea to Wow</div>
+                                    <div className="blog-oneliner">The MVP Story Explained!</div>
+                                </div>
+                                <div className="author-info align-items-center pt-5 mt-2">
+                                    <div className="auth-img">
+                                        <Image src={Author} alt="author-image" height={50} />
+                                    </div>
+                                    <div className="px-2">
+                                        <div className="author-name">Indira</div>
+                                        <div className="publish-date">{today}</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-
-                    <div className="blog-item col-lg-4">
-                        <a href="blogs/vision" className="text-decoration-none text-black">
-                            <div className="blog-img">
-                                <Image src={Vision} alt="MVP landing" className="blog-img-box" />
-                            </div>
-                            <div className="blog-details">
-                                <div className="blog-genre pt-3">Story</div>
-                                <div className="blog-title py-3">From Vision to MVP</div>
-                                <div className="blog-oneliner">Uncover the hurdles non-tech founders confront when crafting an MVP in 100 days, and find actionable solutions in the upcoming blog.</div>
-                            </div>
-                            <div className="author-info align-items-center pt-3">
-                                <div className="auth-img"><Image src={Author} alt="author-image" width={30} /></div>
-                                <div className="px-2">
-                                    <div className="author-name">Indira</div>
-                                    <div className="publish-date">January 01,2023</div>
-                                </div>
-                            </div>
-                        </a>
-                    </div> */}
                 </div>
 
-{/* 
-                <div className="blog-row row mb-5">
-                    
-                </div> */}
             </Container>
         </div>
     )
